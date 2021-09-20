9020c73c0b50d99be13a08e1fc698f279b8f9124
Anandkrishnan V S <vs.anandkrishnan@outlook.com>
2021-09-20 08:40:52 +0530

    Added Reporter Project

---
+++ Reporter/Reporter.cs
@@ -0,0 +1,56 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports.Reporter.Configuration;
+
+namespace APIApplication
+{
+    public static class Reporter
+    {
+        public static ExtentReports extendReports;
+        public static ExtentReports extent;
+        public static ExtentHtmlReporter htmlReporter;
+        public static ExtentTest testCase;
+
+        public static void SetUpExtentReport(string reportName, string  documentTitle, dynamic path)
+        {
+            htmlReporter = new ExtentHtmlReporter(path);
+            htmlReporter.Config.Theme = Theme.Standard;
+            htmlReporter.Config.DocumentTitle = documentTitle;
+            htmlReporter.Config.ReportName = reportName;
+
+
+            extent = new ExtentReports();
+            extent.AttachReporter(htmlReporter);
+            extendReports = extent;
+
+        }
+
+        public static void CreateText(string testName)
+        {
+            testCase = extendReports.CreateTest(testName);
+        }
+
+        public static void LogToReport(Status status,string message)
+        {
+            testCase.Log(status,message);
+        }
+
+        public static void FlushReport()
+        {
+            extendReports.Flush();
+        }
+
+        public static void TestStatus(string status)
+        {
+            if (status.Equals("Pass"))
+            {
+                testCase.Pass("Test case is passed");
+            }
+            else
+            {
+                testCase.Fail("Test case is failed");
+
+            }
+        }
+    }
+}
--- Tests/APITest.cs
+++ Tests/APITest.cs
@@ -1,13 +1,28 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using APIApplication;
-using APIApplication.Helpers;
 using APIApplication.JsonObjects;
+using AventStack.ExtentReports;
 
 namespace Tests
 {
     [TestClass]
     public class APITest
     {
+        public TestContext TestContext { get; set; }
+
+        [ClassInitialize]
+        public static void SetUp(TestContext testContext)
+        {
+            var dir = testContext.TestRunDirectory;
+            Reporter.SetUpExtentReport("API Regression Test", "API Regression Test", dir);
+        }
+
+        [TestInitialize]
+        public void SetUpTest()
+        {
+            Reporter.CreateText(TestContext.TestName);
+        }
+
         [TestMethod]
         public void APIGetTest()
         {
@@ -26,7 +41,40 @@ namespace Tests
             var handleAPI = new HandleAPI<CreateUserResponse>();
             var deserializeContent = handleAPI.CreateUser("api/users", payLoad);
             Assert.AreEqual("morpheus", deserializeContent.name);
+            Reporter.LogToReport(Status.Info, "First Name Does not Match");
             Assert.AreEqual("leader", deserializeContent.job);
+            Reporter.LogToReport(Status.Info, "Job Description Does Not Match");
         }
+
+
+        [TestCleanup]
+        public void CleanUpTest()
+        {
+            var testStatus = TestContext.CurrentTestOutcome;
+            Status logStatus;
+
+            switch (testStatus)
+            {
+                case UnitTestOutcome.Failed:
+                    logStatus = Status.Fail;
+                    Reporter.TestStatus(logStatus.ToString());
+                    break;
+                case UnitTestOutcome.Passed:
+                    logStatus = Status.Pass;
+                    Reporter.TestStatus(logStatus.ToString());
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        [ClassCleanup]
+        public static void CleanUp() {
+
+            Reporter.FlushReport();
+        }   
+
+
+
     }
 }
