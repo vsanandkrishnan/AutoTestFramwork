476f12f45da1bb54499f2a678853a81ec30c0d80
Anandkrishnan V S <vs.anandkrishnan@outlook.com>
2021-09-20 07:39:53 +0530

    Added Extend reporter class to API Project

---
+++ APIApplication/Reporter.cs
@@ -0,0 +1,33 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports.Reporter.Configuration;
+
+namespace APIApplication
+{
+    public static class Reporter
+    {
+        public static ExtentReports extendReports;
+        public static ExtentReports extent;
+        public static ExtentHtmlReporter htmlReporter;
+        public static ExtentTest textCase;
+
+        public static void SetUpExtentReport(string reportName, string  documentTitle, dynamic path)
+        {
+            htmlReporter = new ExtentHtmlReporter(path);
+            htmlReporter.Config.Theme = Theme.Dark;
+            htmlReporter.Config.DocumentTitle = documentTitle;
+            htmlReporter.Config.ReportName = reportName;
+
+
+            extent = new ExtentReports();
+            extent.AttachReporter(htmlReporter);
+            extendReports = extent;
+
+
+
+
+
+        }
+
+    }
+}
