c2b40379bc89eaab277c90dd31bb166372445045
Anandkrishnan V S <vs.anandkrishnan@outlook.com>
2021-09-24 09:22:10 +0530

    Added XML reader configure files

---
+++ EnvironmentConfiguration/Configurationfiles/UserDetails.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace EnvironmentConfiguration.Configurationfiles
+{
+    [Serializable]
+    [XmlRoot(ElementName="UserDetails")]
+    public class UserDetails
+    {
+        [XmlElement(ElementName ="User")]
+        public List<User> userList = new List<User>();
+
+    }
+
+    [Serializable]
+    [XmlRoot(ElementName ="User")]
+    public class User
+    {
+        [XmlElement(ElementName ="UserId")]
+        public int UserId { get; set; }
+
+        [XmlElement(ElementName = "UserName")]
+        public string UserName { get; set; }
+
+        [XmlElement(ElementName = "Password")]
+        public string Password { get; set; }
+    }
+}
--- EnvironmentConfiguration/Settings.cs
+++ EnvironmentConfiguration/Settings.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using EnvironmentConfiguration.Configurationfiles;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace EnvironmentConfiguration
 {
@@ -25,4 +27,24 @@ namespace EnvironmentConfiguration
             configuration.Bind(config);
         }
     }
+
+    public class XMlConfiguration
+    {
+        public static string projectPath = Environment.CurrentDirectory.Substring(0, Environment.CurrentDirectory.IndexOf("AutoTestFramework"));
+        public static string configSettingPath = @"AutoTestFramework\EnvironmentConfiguration\Configuration\UserFiles.xml";
+        public static string configPath = Path.Combine(projectPath, configSettingPath);
+
+        public static UserDetails GetUserDetails()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(UserDetails));
+            TextReader reader = new StreamReader(configPath);
+            object obj = deserializer.Deserialize(reader);
+            UserDetails XmlData = (UserDetails)obj;
+            reader.Close();
+            return XmlData;
+        }
+
+    }
+
+    
 }
