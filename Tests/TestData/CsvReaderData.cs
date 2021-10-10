306b7eb0cd44072f05e0f843bdccf9fbd6239849
Anandkrishnan V S <vs.anandkrishnan@outlook.com>
2021-10-10 17:55:41 +0530

    Working on CSV reader

---
+++ Tests/TestData/CsvReaderData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CsvHelper;
+using System.IO;
+using System.Globalization;
+using System.Linq;
+
+namespace Tests.TestData
+{
+    class CsvReaderData
+    {
+    }
+
+    public void CsvReaderMethod()
+    {
+       var streamReadeer = new StreamReader
+    }
+}
